be42b267bcda7530e5e69c84f03d0e957c024ec3
Piston <yoba123@yandex.ru>
2016-02-28 07:00:11 +0300

    github asset shortcut

--- src/main/scala/com/karasiq/scalajsbundler/dsl/package.scala
+++ src/main/scala/com/karasiq/scalajsbundler/dsl/package.scala
@@ -1,6 +1,6 @@
 package com.karasiq.scalajsbundler
 
-package object dsl extends BundlerDsl {
+package object dsl extends BundlerDsl with AssetShortcuts {
   type Bundle = ScalaJSBundler.Bundle
   val Bundle: ScalaJSBundler.Bundle.type = ScalaJSBundler.Bundle
 }
