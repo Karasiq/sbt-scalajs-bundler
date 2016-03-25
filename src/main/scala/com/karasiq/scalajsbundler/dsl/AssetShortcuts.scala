b2be7473c89ba24c58349d8f2e2765ec5bca4e8d
Piston <yoba123@yandex.ru>
2016-03-25 19:46:55 +0300

    scala.js application shortcut

--- src/main/scala/com/karasiq/scalajsbundler/dsl/AssetShortcuts.scala
+++ src/main/scala/com/karasiq/scalajsbundler/dsl/AssetShortcuts.scala
@@ -3,6 +3,8 @@ package com.karasiq.scalajsbundler.dsl
 import java.net.URL
 
 import com.karasiq.scalajsbundler.ScalaJSBundler.{PageContent, ResourceAsset}
+import sbt.Keys._
+import sbt._
 
 trait AssetShortcuts { self: BundlerDsl ⇒
   def fontPackage(name: String, baseUrl: String, dir: String = "fonts", extensions: Seq[String] = Seq("eot", "svg", "ttf", "woff", "woff2")): Seq[PageContent] = {
@@ -17,5 +19,18 @@ trait AssetShortcuts { self: BundlerDsl ⇒
 
   def resource(path: String): ResourceAsset = {
     ResourceAsset(path)
+  }
+
+  def scalaJsApplication(project: Project, launcher: Boolean = true, fastOpt: Boolean = false) = {
+    (name in project, target in project, scalaVersion in project) {
+      case (name, target, version) ⇒
+        val output = target / s"scala-${CrossVersion.binaryScalaVersion(version)}"
+        var files = Vector.empty[PageContent]
+        if (fastOpt) files :+= Script from output / s"$name-fastopt.js"
+        else files :+= Script from output / s"$name-opt.js"
+
+        if (launcher) files :+= Script from output / s"$name-launcher.js"
+        files
+    }
   }
 }
